368f40e15c9042cf3ba1c79611047f02ade7f0ba
Aleksandra Ciprijanovic <43037641+AleksCipri@users.noreply.github.com>
2021-08-05 16:40:16 -0500

    updated functions with embeddings

--- isomap_plot.py
+++ isomap_plot.py
@@ -12,150 +12,194 @@ import torch.nn.functional as F
 
 
 
-def isomap_plot(base_network, img_reg, img_noise, img_pert, num, three_dim=True, save=False, where=False):
-  
-  ind = np.random.randint(0,len(img_reg),num)
-  num2 = 2 * num
+def isomap_plot(embed_network, embed_reg, embed_nois, embed_pert, base_network, img_reg, img_noise, num, three_dim=True, save=False, where=False): 
+    np.random.seed(0)
+    ind = np.random.randint(0,len(img_reg),num)
+    num2 = 2 * num
+    num3 = num2+ len(embed_reg)
+    num4 = num3 + len(embed_nois)
+    #print(num,num2,num3,num4)
+
+    #Make embeddings for many images to build Isomap space  
+    base_network.to('cpu')
+    embedding_reg, logits_reg = base_network(img_reg[ind])
+    embedding_noise, logits_noise = base_network(img_noise[ind])
+
+    softmax_reg = nn.Softmax(dim=1)(1.0 * logits_reg)
+    _, predict_reg = torch.max(softmax_reg, 1)
+    softmax_noi = nn.Softmax(dim=1)(1.0 * logits_noise)
+    _, predict_noi = torch.max(softmax_noi, 1)
+
+    predict_reg = predict_reg.tolist()
+    predict_noi = predict_noi.tolist()
+
+
+    # Import embedings for selected attacked triplets
+    embed_network.to('cpu')
+    embed_reg = embed_reg.detach().squeeze().to('cpu')
+    embed_nois = embed_nois.detach().squeeze().to('cpu')
+    embed_pert = embed_pert.detach().squeeze().to('cpu')
     
-  base_network.to('cpu')
-  embedding_reg, logits_reg = base_network(img_reg[ind])
-  embedding_noise, logits_noise = base_network(img_noise[ind])
-  embedding_pert, logits_pert = base_network(img_pert)
-
-  total = []
-  total.append(embedding_reg)
-  total.append(embedding_noise)
-  total.append(embedding_pert)
-
-  total = torch.cat(total)
-  len(total)
-
-  softmax_reg = nn.Softmax(dim=1)(1.0 * logits_reg)
-  _, predict_reg = torch.max(softmax_reg, 1)
-
-  softmax_noi = nn.Softmax(dim=1)(1.0 * logits_noise)
-  _, predict_noi = torch.max(softmax_noi, 1)
-
-  softmax_pert = nn.Softmax(dim=1)(1.0 * logits_pert)
-  _, predict_pert = torch.max(softmax_pert, 1)
+    log_reg = embed_network(embed_reg)
+    sm_reg = nn.Softmax(dim=1)(1.0*log_reg)
+    _, pr_reg = torch.max(sm_reg, 1)
+    
+    log_noi = embed_network(embed_nois)
+    sm_noi = nn.Softmax(dim=1)(1.0*log_noi)
+    _, pr_noi = torch.max(sm_noi, 1)
+    
+    log_pert = embed_network(embed_pert)
+    sm_pert = nn.Softmax(dim=1)(1.0*log_pert)
+    _, pr_pert = torch.max(sm_pert, 1)
 
 
-  predict_reg = predict_reg.tolist()
-  predict_noi = predict_noi.tolist()
-  predict_pert = predict_pert.tolist()
+    total = []
+    total.append(embedding_reg)
+    total.append(embedding_noise)
+    total.append(embed_reg)
+    total.append(embed_nois)
+    total.append(embed_pert)
 
-  color = np.array(['r', 'b', 'k'])
+    total = torch.cat(total)
 
-  if three_dim:
-    model = Isomap(n_components=3)
+    color = np.array(['r', 'b', 'k'])
+
+
+    if three_dim:
+        model = Isomap(n_components=3)
+        proj = model.fit_transform(total.detach().cpu().numpy())
+
+        
+        fig = plt.figure(figsize = (20, 10))
+        ax = plt.gca()
+        ax = plt.axes(projection='3d')
+        plt.title('Isomap 3D \n Curves: Y10 (orange), Y1 noisy (blue) \n Point color - predicted class: spiral (red), elliptical (blue), merger (black) \n Perturbed images - large points: Y10 image (+), Y1  noisy (*), perturbed image (^)', fontsize=25)
+
+        ax.plot_trisurf(proj[:num, 0], proj[:num, 1], proj[:num, 2], cmap="autumn", alpha = 0.2)
+        ax.plot_trisurf(proj[num:num2, 0], proj[num:num2, 1], proj[num:num2, 2],cmap="winter", alpha=0.2)
+        ax.scatter(proj[:num, 0], proj[:num, 1], proj[:num, 2], c=color[predict_reg[:]],marker='o', s=20)
+        ax.scatter(proj[num:num2, 0], proj[num:num2, 1], proj[num:num2, 2],c=color[predict_noi[:]],marker='o', s=20)
+
+        ax.scatter(proj[num2:num3, 0], proj[num2:num3, 1], proj[num2:num3, 2],c=color[pr_reg[:]],marker='+', s=400)
+        ax.scatter(proj[num3:num4, 0], proj[num3:num4, 1], proj[num3:num4, 2],c=color[pr_noi[:]],marker='*', s=400)
+        ax.scatter(proj[num4:, 0], proj[num4:, 1], proj[num4:, 2],c=color[pr_pert[:]],marker='^', s=400)
+
+    else:
+        model = Isomap(n_components=2)
+        proj = model.fit_transform(total.detach().cpu().numpy())
+
+        fig = plt.figure(figsize = (20, 10))
+        ax = plt.gca()
+        plt.title('Isomap 2D \n Point color - predicted class: spiral (red), elliptical (blue), merger (black) \n Test images: Y10 large, Y1 noisy small \n Perturbed images - large points: Y10 image (+), Y1  noisy (*), perturbed image (^)', fontsize=25)
+
+        ax.scatter(proj[:num, 0], proj[:num, 1], c=color[predict_reg[:]],marker='o', s=80)
+        ax.scatter(proj[num:num2, 0], proj[num:num2, 1] ,c=color[predict_noi[:]],marker='o', s=20)
+
+        ax.scatter(proj[num2:num3, 0], proj[num2:num3, 1],c=color[pr_reg[:]],marker='+', s=400)
+        ax.scatter(proj[num3:num4, 0], proj[num3:num4, 1],c=color[pr_noi[:]],marker='*', s=400)
+        ax.scatter(proj[num4:, 0], proj[num4:, 1],c=color[pr_pert[:]],marker='^', s=400)
+
+
+    if save:
+        plt.savefig(where+'.png')
+        
+        
+        
+def isomap_video(embed_network, embed_reg, embed_nois, embed_pert, base_network, img_reg, img_noise, num, vert=False, save=False, where=False):
+    np.random.seed(0)
+    ind = np.random.randint(0,len(img_reg),num)
+    num2 = 2 * num
+    num3 = num2+ len(embed_reg)
+    num4 = num3 + len(embed_nois)
+    #print(num,num2,num3,num4)
+
+    #Make embeddings for many images to build Isomap space  
+    base_network.to('cpu')
+    embedding_reg, logits_reg = base_network(img_reg[ind])
+    embedding_noise, logits_noise = base_network(img_noise[ind])
+
+    softmax_reg = nn.Softmax(dim=1)(1.0 * logits_reg)
+    _, predict_reg = torch.max(softmax_reg, 1)
+    softmax_noi = nn.Softmax(dim=1)(1.0 * logits_noise)
+    _, predict_noi = torch.max(softmax_noi, 1)
+
+    predict_reg = predict_reg.tolist()
+    predict_noi = predict_noi.tolist()
+
+
+    # Import embedings for selected attacked triplets
+    embed_network.to('cpu')
+    embed_reg = embed_reg.detach().squeeze().to('cpu')
+    embed_nois = embed_nois.detach().squeeze().to('cpu')
+    embed_pert = embed_pert.detach().squeeze().to('cpu')
+    
+    log_reg = embed_network(embed_reg)
+    sm_reg = nn.Softmax(dim=1)(1.0*log_reg)
+    _, pr_reg = torch.max(sm_reg, 1)
+    
+    log_noi = embed_network(embed_nois)
+    sm_noi = nn.Softmax(dim=1)(1.0*log_noi)
+    _, pr_noi = torch.max(sm_noi, 1)
+    
+    log_pert = embed_network(embed_pert)
+    sm_pert = nn.Softmax(dim=1)(1.0*log_pert)
+    _, pr_pert = torch.max(sm_pert, 1)
+
+
+    total = []
+    total.append(embedding_reg)
+    total.append(embedding_noise)
+    total.append(embed_reg)
+    total.append(embed_nois)
+    total.append(embed_pert)
+
+    total = torch.cat(total)
+
+    color = np.array(['r', 'b', 'k'])
+
+    X = np.arange(-5, 5, 0.25)
+    Y = np.arange(-5, 5, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    R = np.sqrt(X**2 + Y**2)
+    Z = np.sin(R)
 
     fig = plt.figure(figsize = (20, 10))
     ax = plt.gca()
     ax = plt.axes(projection='3d')
-    plt.title('Isomap 3D \n Curves: Y10 (orange), Y1 noisy (blue) \n Point color - predicted class: spiral (red), elliptical (blue), merger (black) \n Perturbed images - large points', fontsize=25)
+    plt.title('Isomap 3D \n Curves: Y10 (orange), Y1 noisy (blue) \n Point color - predicted class: spiral (red), elliptical (blue), merger (black) \n Perturbed images - large points: Y10 image (+), Y1  noisy (*), perturbed image (^)', fontsize=25)
 
-    proj = model.fit_transform(total.detach().numpy())
-    ax.plot_trisurf(proj[:num, 0], proj[:num, 1], proj[:num, 2], cmap="autumn", alpha = 0.3)
-    ax.plot_trisurf(proj[num:num2, 0], proj[num:num2, 1], proj[num:num2, 2],cmap="winter", alpha=0.3)
-    ax.plot_trisurf(proj[num2:, 0], proj[num2:, 1], proj[num2:, 2],cmap="Reds", alpha=0.3)
+    def init():
+        model = Isomap(n_components=3)
+        proj = model.fit_transform(total.detach().numpy())
 
-    ax.scatter(proj[:num, 0], proj[:num, 1], proj[:num, 2], c=color[predict_reg[:]],marker='o', s=40)
-    ax.scatter(proj[num:num2, 0], proj[num:num2, 1], proj[num:num2, 2],c=color[predict_noi[:]],marker='o', s=40)
-    ax.scatter(proj[num2:, 0], proj[num2:, 1], proj[num2:, 2],c=color[predict_pert[:]],marker='^', s=250)
+        ax.plot_trisurf(proj[:num, 0], proj[:num, 1], proj[:num, 2], cmap="autumn", alpha = 0.2)
+        ax.plot_trisurf(proj[num:num2, 0], proj[num:num2, 1], proj[num:num2, 2],cmap="winter", alpha=0.2)
+        ax.scatter(proj[:num, 0], proj[:num, 1], proj[:num, 2], c=color[predict_reg[:]],marker='o', s=20)
+        ax.scatter(proj[num:num2, 0], proj[num:num2, 1], proj[num:num2, 2],c=color[predict_noi[:]],marker='o', s=20)
 
-  else:
-    model = Isomap(n_components=2)
+        ax.scatter(proj[num2:num3, 0], proj[num2:num3, 1], proj[num2:num3, 2],c=color[pr_reg[:]],marker='+', s=400)
+        ax.scatter(proj[num3:num4, 0], proj[num3:num4, 1], proj[num3:num4, 2],c=color[pr_noi[:]],marker='*', s=400)
+        ax.scatter(proj[num4:, 0], proj[num4:, 1], proj[num4:, 2],c=color[pr_pert[:]],marker='^', s=400)
+        return fig,
 
-    fig = plt.figure(figsize = (20, 10))
-    ax = plt.gca()
-    plt.title('Isomap 2D \n Point color - predicted class: spiral (red), elliptical (blue), merger (black) \n Perturbed images - large points', fontsize=25)
+    if vert:
+        def animate(i):
+          # elevation angle : -180 deg to 180 deg
+          ax.view_init(elev=(i-45)*4, azim=10)
+          return fig,
+    else:
+        def animate(i):
+          # azimuth angle : 0 deg to 360 deg
+          ax.view_init(elev=10, azim=i*4)
+          return fig,
 
-    proj = model.fit_transform(total.detach().numpy())
-    ax.scatter(proj[:num, 0], proj[:num, 1], c=color[predict_reg[:]],marker='o', s=40)
-    ax.scatter(proj[num:num2, 0], proj[num:num2, 1] ,c=color[predict_noi[:]],marker='o', s=40)
-    ax.scatter(proj[num2:, 0], proj[num2:, 1], c=color[predict_pert[:]],marker='^', s=250)
-
-  if save:
-      plt.savefig(where+'.png')
-
-
-
-
-def isomap_video(base_network, img_reg, img_noise, img_pert, num, vert=True, save=False, where=False):
-    
-  ind = np.random.randint(0,len(img_reg),num)
-  num2 = 2 * num
-    
-  base_network.to('cpu')
-  embedding_reg, logits_reg = base_network(img_reg[ind])
-  embedding_noise, logits_noise = base_network(img_noise[ind])
-  embedding_pert, logits_pert = base_network(img_pert)
-
-  total = []
-  total.append(embedding_reg)
-  total.append(embedding_noise)
-  total.append(embedding_pert)
-
-  total = torch.cat(total)
-  len(total)
-
-  softmax_reg = nn.Softmax(dim=1)(1.0 * logits_reg)
-  _, predict_reg = torch.max(softmax_reg, 1)
-
-  softmax_noi = nn.Softmax(dim=1)(1.0 * logits_noise)
-  _, predict_noi = torch.max(softmax_noi, 1)
-
-  softmax_pert = nn.Softmax(dim=1)(1.0 * logits_pert)
-  _, predict_pert = torch.max(softmax_pert, 1)
-
-
-  predict_reg = predict_reg.tolist()
-  predict_noi = predict_noi.tolist()
-  predict_pert = predict_pert.tolist()
-
-  color = np.array(['r', 'b', 'k'])
-
-  X = np.arange(-5, 5, 0.25)
-  Y = np.arange(-5, 5, 0.25)
-  X, Y = np.meshgrid(X, Y)
-  R = np.sqrt(X**2 + Y**2)
-  Z = np.sin(R)
-
-  fig = plt.figure(figsize = (20, 10))
-  ax = plt.gca()
-  ax = plt.axes(projection='3d')
-  plt.title('Isomap 3D \n Curves: Y10 (orange), Y1 noisy (blue) \n Point color - predicted class: spiral (red), elliptical (blue), merger (black) \n Perturbed images - large points', fontsize=25)
-
-  def init():
-    model = Isomap(n_components=3)
-    proj = model.fit_transform(total.detach().numpy())
-
-    ax.plot_trisurf(proj[:num, 0], proj[:num, 1], proj[:num, 2], cmap="autumn", alpha = 0.3)
-    ax.plot_trisurf(proj[num:num2, 0], proj[num:num2, 1], proj[num:num2, 2],cmap="winter", alpha=0.3)
-    ax.plot_trisurf(proj[num2:, 0], proj[num2:, 1], proj[num2:, 2],cmap="Reds", alpha=0.3)
-
-    ax.scatter(proj[:num, 0], proj[:num, 1], proj[:num, 2], c=color[predict_reg[:]],marker='o', s=40)
-    ax.scatter(proj[num:num2, 0], proj[num:num2, 1], proj[num:num2, 2],c=color[predict_noi[:]],marker='o', s=40)
-    ax.scatter(proj[num2:, 0], proj[num2:, 1], proj[num2:, 2],c=color[predict_pert[:]],marker='^', s=250)
-    return fig,
-
-  if vert:
-    def animate(i):
-      # elevation angle : -180 deg to 180 deg
-      ax.view_init(elev=(i-45)*4, azim=10)
-      return fig,
-  else:
-    def animate(i):
-      # azimuth angle : 0 deg to 360 deg
-      ax.view_init(elev=10, azim=i*4)
-      return fig,
-
-  # Animate
-  ani = animation.FuncAnimation(fig, animate, init_func=init,
+    # Animate
+    ani = animation.FuncAnimation(fig, animate, init_func=init,
                             frames=90, interval=50, blit=True)
 
-  if save:
-    fn = where
-    ani.save(fn+'.mp4',writer='ffmpeg',fps=1000/50)
+    if save:
+        fn = where
+        ani.save(fn+'.mp4',writer='ffmpeg',fps=1000/50)
   
-  return ani
+    return ani
+
